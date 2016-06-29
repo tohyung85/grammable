7b908a2d2e0ca24e07a57665bf1c001a0c5358ae
Your Name <tantohyung@gmail.com>
2016-06-29 13:35:37 +0000

    style page

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,6 +2,6 @@ class Gram < ActiveRecord::Base
   validates :message, presence: true
   validates :picture, presence: true
   belongs_to :user
-  has_many :comments
+  has_many :comments , dependent: :destroy
   mount_uploader :picture, PictureUploader
 end
